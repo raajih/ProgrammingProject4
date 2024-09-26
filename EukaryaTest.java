19f961a354aee533df8abaff7920dbdcf6e8f01f
Raajih Roland <raajihroland88@gmail.com>
2024-09-26 18:16:43 -0400

    JUnit practice

--- EukaryaTest.java
+++ EukaryaTest.java
@@ -1,11 +1,20 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class EukaryaTest {
-    public static void main(String[] args) {
+    @Test
+     public void testAnt()
+     {
         Eukarya testAnt = new Ant("phylum", "class", "order", "family", "genus", "species", "Anty the ant", false, "worker");
-        
+        assertEquals("species", testAnt.getSpecies());
+     }
+
+/* 
         Eukarya testAardvark = new Aardvark("phylum", "class", "order", "family", "genus", "species", "Arty the aardvark", true, "Dessert");
 
         Eukarya testVenus = new VenusFlyTrap("division", "class", "order", "family", "genus", "species", "Veny the venus", true, 4);
 
         System.out.print(testAnt.toString() + "\n\n" + testAardvark.toString() + "\n\n" + testVenus.toString());
-    }
+    */
 }
